ce6c12854c0339a012f0ebc2756ac6a9553d386d
lixiaoyao711 <1176286624@qq.com>
2022-03-24 11:37:10 +0800

    添加axios拦截器

--- src/main.js
+++ src/main.js
@@ -2,12 +2,32 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import axios from 'axios';
 import '../src/assets/css/global.css';
-
 
 import ElementPlus from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import 'element-plus/dist/index.css';
+
+axios.defaults.baseURL = '/api';
+
+// 请求拦截器
+axios.interceptors.request.use(config => {
+  return config;
+});
+// 响应拦截器
+axios.interceptors.response.use(
+  res => {
+    // let message = Vue.prototype.$message
+    if (res.data.code === 200) {
+      return res.data;
+    }
+  },
+  err => {
+    console.error(err);
+    return Promise.reject(err);
+  }
+);
 
 createApp(App)
   .use(ElementPlus, {
