ef8c87c84a7e66230dbff77d872f99b43971e8e9
lixiaoyao711 <1176286624@qq.com>
2022-08-26 11:28:58 +0800

    fix--完善初始化验证文件

--- src/utils/regular.js
+++ src/utils/regular.js
@@ -6,6 +6,7 @@ export function regMobile(type, str = navigator.userAgent) {
     iphone: /.*iPhone.*/, // iPhone
     android: /(.*Linux+.*Android+.*)|(.*Android+.*Linux+.*)/, // Android
     weibo: /.*Weibo.*/, // 微博
+    dtdreamweb: /.*dtdreamweb.*/, // 浙里办
 
     wechat: /.*(MicroMessenger|micromessenger)+.*/, // 微信
     pcwechat: /.*WindowsWechat+.*/, // 微信
@@ -28,7 +29,7 @@ export var checkPhone = (rule, value, cb) => {
   //验证手机号是否合法
   const regChinese = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
   if (regChinese.test(value)) {
-    return true
+    return true;
   }
-  return false
+  return false;
 };
